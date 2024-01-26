9090cda1624b6c03949fc6dcb5d06bcab2852d1e
Nekroid <philipleinung@aol.de>
2024-01-26 10:48:09 +0100

    boolean is female

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,19 +1,22 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat
+{
 
   private String name;
   private String furColor;
   private int age; 
+  private boolean isFemale;
 
 
-public Cat(String name, String furColor, int age) {
+  public Cat(String name, String furColor, int age, boolean isFemale) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
+    this.isFemale = isFemale;
   }
 
-public String getStringAttributes(String op) {
+  public String getStringAttributes(String op) {
   switch (op) {
     case "#name":
       return name;
@@ -24,10 +27,18 @@ public String getStringAttributes(String op) {
   }
 }
 
-public String getAge() {
-    return String.valueOf(age);
+  public String getAge()
+  {
+    if (isFemale)
+    {
+      return "This is an inappropriate question!";
+      
+    }
+    else
+    {
+      return String.valueOf(age);
+    }
   }
-
 
 }
   
